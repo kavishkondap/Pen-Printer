00d5cccf2dac70341d993394e599aa7faada7e83
Preston Seay <preston.seay@outlook.com>
2022-05-31 14:48:40 -0700

    fully fudged

--- python_program/img_processing.py
+++ python_program/img_processing.py
@@ -8,152 +8,167 @@ import time
 
 
 def send_instructions(instructions):
-	serialcomm = serial.Serial('COM7', 9600)  # todo: try increasing baud rate
-	serialcomm.timeout = 0.3
-	total = len(instructions)
-	progress = 0
-	serialcomm.write("tell me you see meeeeeee (⓿_⓿)".encode())
+    serialcomm = serial.Serial('COM7', 9600)  # todo: try increasing baud rate
+    serialcomm.timeout = 0.3
+    total = len(instructions)
+    progress = 0
+    serialcomm.write("tell me you see meeeeeee (⓿_⓿)".encode())
 
-	# wait for manual reset
-	print("Put pen down on the paper")
-	input("Press enter to continue...")
+    # wait for manual reset
+    print("Put pen down on the paper")
+    input("Press enter to continue...")
 
-	for instruction in instructions:
-		#send instruction
-		serialcomm.write(instruction.encode())
-		print(instruction)
+    for instruction in instructions:
+        # send instruction
+        serialcomm.write(instruction.encode())
+        print(instruction)
 
-		#wait for response that it finished
-		message = serialcomm.readline().decode()
-		while message.startswith("executed:") == False:
-			message = serialcomm.readline().decode()
+        # wait for response that it finished
+        message = serialcomm.readline().decode()
+        while message.startswith("executed:") == False:
+            message = serialcomm.readline().decode()
 
-		#show execution result
-		print(message)
+        # show execution result
+        print(message)
 
-		#show progress
-		progress += 1
-		print("progress: " + str(math.floor(100*progress/total)) + "%")
+        # show progress
+        progress += 1
+        print("progress: " + str(math.floor(100*progress/total)) + "%")
 
-	print("done... ✍(◔◡◔)")
+    print("done... ✍(◔◡◔)")
 
-	return
+    return
 
 
 def make_instructions(frame):
-	# defining variables
-	margin = 0.5
-	steps_per_inch = 2000/16
+    # defining variables
+    margin = 0.5
+    steps_per_inch = 2000/16
 
-	width, height = frame.shape
-	steps_per_pixel_horizontal = steps_per_inch * ((8.5 - (margin*2)) / width)
-	steps_per_pixel_vertical = steps_per_inch * ((11 - (margin*2)) / height)
-	steps_per_pixel_preserved = min(
-		steps_per_pixel_horizontal, steps_per_pixel_vertical)
+    width, height = frame.shape
+    steps_per_pixel_horizontal = steps_per_inch * ((8.5 - (margin*2)) / width)
+    steps_per_pixel_vertical = steps_per_inch * ((11 - (margin*2)) / height)
+    steps_per_pixel_preserved = min(
+        steps_per_pixel_horizontal, steps_per_pixel_vertical)
 
-	total = np.sum([[1 if frame[x, y] == 0 else 0 for x in range(width)]
-                 for y in range(height)])
-	calculated = 0
-	print(total)
+    total = np.sum([[1 if frame[x, y] == 0 else 0 for x in range(width)]
+                    for y in range(height)])
+    calculated = 0
+    print(total)
 
-	# calculating instructions
-	global instructions
-	global penHeight
-	global penInitial
-	global penFudge
-	penHeight = 7
-	penInitial = 7
-	penFudge = 3
-	instructions = ["aReset", "penSet " + str(penHeight) + " 10", "penUp"]
+    # calculating instructions
+    global instructions
+    global penHeight
+    global penInitial
+    global penFudge
+    penHeight = 26
+    penInitial = penHeight
+    penFudge = 14
+    instructions = ["penUp", "aReset",
+                    "penSet " + str(penHeight) + " 10", "penUp"]
 
-	global x
-	global y
-	global xSteps
-	global ySteps
-	x = 0
-	y = 0
-	xSteps = 0
-	ySteps = 0
+    global x
+    global y
+    global xSteps
+    global ySteps
+    x = 0
+    y = 0
+    xSteps = 0
+    ySteps = 0
 
-	def stepTo(xPix, yPix):
-		global xSteps
-		global ySteps
+    def stepTo(xPix, yPix):
+        global xSteps
+        global ySteps
+        global penHeight
 
-		xGoalSteps = xPix * steps_per_pixel_horizontal
-		yGoalSteps = yPix * steps_per_pixel_vertical
+        xGoalSteps = xPix * steps_per_pixel_horizontal
+        yGoalSteps = yPix * steps_per_pixel_vertical
 
-		xDifSteps = math.floor(xGoalSteps - xSteps)
-		yDifSteps = math.floor(yGoalSteps - ySteps)
+        xDifSteps = math.floor(xGoalSteps - xSteps)
+        yDifSteps = math.floor(yGoalSteps - ySteps)
 
-		instructions.append("aMove 0," + str(xDifSteps * 16) +
-		                    ";0," + str(16*yDifSteps))
-		xSteps += xDifSteps
-		ySteps += yDifSteps
+        xSteps += xDifSteps
+        ySteps += yDifSteps
 
-		change = math.floor(x * penFudge/width)
-		penGoal = penInitial - change
-		if penGoal != penHeight:
-			penHeight = penGoal
-			instructions.append("penSet " + str(penHeight) + " " +
-			                    str(10 + math.floor(change/2)))
-		instructions.append("penPress")
+        change = math.floor(x * penFudge/width)
+        penGoal = penInitial + change
+        #moves the pen up (first) if it is going out more
+        if penGoal > penHeight:
+            penHeight = penGoal
+            instructions.append("penSet " + str(penHeight) + " " +
+                        str(10 + math.floor(change/2)))
 
-		return
+        #moves the pen to the next spot
+        instructions.append("aMove 0," + str(xDifSteps * 16) +
+                    ";0," + str(16*yDifSteps))
 
-	visited = [[1 if c % 2 == 0 or r %
-             2 == 0 else 0 for c in range(height)] for r in range(width)]
-	global instructionWasMade
-	instructionWasMade = True
-	while calculated < total and instructionWasMade:
-		instructionWasMade = False
-		# find nearest undrawn pixel
-		xOff = 0
-		yOff = 0
-		for num in range(1, max(x, width-x, y, height-y)):
-			dir = -1 if num % 2 == 0 else 1
-			checkY = y + yOff
-			for dXOff in range(0, num):
-				xOff += dir
-				checkX = x + xOff
-				#check out of bounds
-				if checkX < 0 or checkX >= width or checkY < 0 or checkY >= height:
-					continue
-				#check pixel
-				if frame[checkX, checkY] == 0 and visited[checkX][checkY] == 0:
-					visited[checkX][checkY] = 1
-					x = checkX
-					y = checkY
-					stepTo(x, y)
-					calculated += 1
-					print("processing: " + str(math.floor(100*calculated/total)) + "%")
-					instructionWasMade = True
-					break
-			if instructionWasMade:
-				break
-			checkX = x + xOff
-			for dYOff in range(0, num):
-				yOff += dir
-				checkY = y + yOff
-				#check out of bounds
-				if checkX < 0 or checkX >= width or checkY < 0 or checkY >= height:
-					continue
-				#check pixel
-				if frame[checkX, checkY] == 0 and visited[checkX][checkY] == 0:
-					visited[checkX][checkY] = 1
-					x = checkX
-					y = checkY
-					stepTo(x, y)
-					calculated += 1
-					print("processing: " + str(math.floor(100*calculated/total)) + "%")
-					instructionWasMade = True
-					break
-			if instructionWasMade:
-				break
+        #moves the pen up (second) if it is going out more
+        if penGoal < penHeight:
+            penHeight = penGoal
+            instructions.append("penSet " + str(penHeight) + " " +
+                str(10 + math.floor(change/2)))
+    
+        #pressses the pen
+        instructions.append("penPress")
 
-	#move pen back instruction
-	instructions.append("aMove 0," + str(-xSteps) + ";0," + str(-ySteps))
+        return
 
-	return instructions
+    visited = [[1 if c % 2 == 0 or r %
+                2 == 0 else 0 for c in range(height)] for r in range(width)]
+    global instructionWasMade
+    instructionWasMade = True
+    while calculated < total and instructionWasMade:
+        instructionWasMade = False
+        # find nearest undrawn pixel
+        xOff = 0
+        yOff = 0
+        for num in range(1, max(x*2, (width-x)*2, y*2, (height-y)*2)):
+            dir = -1 if num % 2 == 0 else 1
+            checkY = y + yOff
+            for dXOff in range(0, num):
+                xOff += dir
+                checkX = x + xOff
+                # check out of bounds
+                if checkX < 0 or checkX >= width or checkY < 0 or checkY >= height:
+                    continue
+                # check pixel
+                if frame[checkX, checkY] == 0 and visited[checkX][checkY] == 0:
+                    visited[checkX][checkY] = 1
+                    x = checkX
+                    y = checkY
+                    stepTo(x, y)
+                    calculated += 1
+                    print("processing: " +
+                          str(math.floor(100*calculated/total)) + "%")
+                    instructionWasMade = True
+                    break
+            if instructionWasMade:
+                break
+            checkX = x + xOff
+            for dYOff in range(0, num):
+                yOff += dir
+                checkY = y + yOff
+                # check out of bounds
+                if checkX < 0 or checkX >= width or checkY < 0 or checkY >= height:
+                    continue
+                # check pixel
+                if frame[checkX, checkY] == 0 and visited[checkX][checkY] == 0:
+                    visited[checkX][checkY] = 1
+                    x = checkX
+                    y = checkY
+                    stepTo(x, y)
+                    calculated += 1
+                    print("processing: " +
+                          str(math.floor(100*calculated/total)) + "%")
+                    instructionWasMade = True
+                    break
+            if instructionWasMade:
+                break
+
+    # move pen back instruction
+    instructions.append("aMove 0," + str(-xSteps*16) + ";0," + str(-ySteps*16))
+
+    return instructions
 
 
 def get_bw_frame():
@@ -207,7 +222,7 @@ def get_bw_frame():
 
 
 if __name__ == "__main__":
-	frame = get_bw_frame()
-	instructions = make_instructions(frame)
-	# instructions = ["penSet 7 10", "penUp", "aMove 0,1000;0,1000", "aMove 0,-1000;0,-1000"]
-	send_instructions(instructions)
+    frame = get_bw_frame()
+    instructions = make_instructions(frame)
+    # instructions = ["penSet 7 10", "penUp", "aMove 0,1000;0,1000", "aMove 0,-1000;0,-1000"]
+    send_instructions(instructions)
